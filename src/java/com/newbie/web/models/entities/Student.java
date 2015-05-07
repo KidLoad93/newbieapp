b218d4fe77c8c0530dc190c045575d5d839c757e
Trong.Bui <trong.buiquoc@gmail.com>
2015-05-07 16:55:49 +0700

    add structure for app

---
+++ src/java/com/newbie/web/models/entities/Student.java
@@ -0,0 +1,15 @@
+package com.newbie.web.models.entities;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author trongbui
+ */
+public class Student {
+    
+}
