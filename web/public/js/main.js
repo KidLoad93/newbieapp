5147f549c46f831a52e49d51a075602f11cbb420
Trong.Bui <trong.buiquoc@gmail.com>
2015-05-09 16:05:39 +0700

    fix main.js

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -3,18 +3,16 @@ console.log($(".search"));
 $(".search").keyup(function () {
     var text = this.value;
     console.log(text);
-    if(text === "") {
-        $(".item").each(function (index) {
-           $(this).show(); 
-        });
-    };
-    
+    $(".item").each(function (index) {
+        $(this).show();
+    });
+
     $(".item").each(function (index) {
         var item = $(this).children("td.2");
-            var patt = new RegExp(text);
-            var isMatch = patt.test(item.text());
-            if (!isMatch)
-                $(this).hide();
-       
+        var patt = new RegExp(text);
+        var isMatch = patt.test(item.text());
+        if (!isMatch)
+            $(this).hide();
+
     });
 });
